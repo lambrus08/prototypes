318953a33edc946418ca6a7c97cbd45619d3f328
ALDO LAMBRUSCHINI <adloans@gmail.com>
2015-11-04 11:00:41 -0800

    done with FS2

--- js_ajax/main.js
+++ js_ajax/main.js
@@ -1,5 +1,5 @@
 //Create GLOBAL variable below here on line 2
-
+var global_result;
 
 $(document).ready(function(){
 	$('button').click(function(){
@@ -7,8 +7,16 @@ $(document).ready(function(){
 		$.ajax({
 			dataType: 'json',
 			url: 'http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topMovies/json',
-			success: function(result) {
+			success: function(result) {global_result = result;
+				global_result = result;
+				console.log("result ", global_result);
+				var firstMovie = global_result['feed']['entry'][0]['im:image'][2];
+				var films = global_result['feed']['entry'];
+				for(i = 0; i < films.length; i++)
+
+				console.log(" movie: ", firstMovie);
 				console.log('AJAX Success function called, with the following result:', result);
+
 
 			}
 		});
